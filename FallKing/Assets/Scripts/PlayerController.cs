938b8392c350cff8bccf33d76f33c9a20738d173
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-05-21 02:53:13 -0700

    Add more to player controller
    Add velocity controll. Player now cannot just fly up no more. Add slower descent if holding up button. Add limit to player move speed.

--- FallKing/Assets/Scripts/PlayerController.cs
+++ FallKing/Assets/Scripts/PlayerController.cs
@@ -7,26 +7,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 0;
+    //[SerializeField] private float speed = 0;
+    [SerializeField] private float glidingAcceleration = 15f;
+    [SerializeField] private float hoverForce = 0.8f;
+    [SerializeField] private float gravityForce = 0.2f;
+    [SerializeField] private float maxMoveMagnitude = 20f;
+
     private Rigidbody2D rigidBody;
-    private float movementX, movementY;
+    private float playerInputX, playerInputY;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.gravityScale = gravityForce;
     }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        playerInputX = movementVector.x;
+        playerInputY = movementVector.y;
     }
 
+
     // Update is called once per frame
+    /// <summary>
+    /// Up = only lower the gravity force
+    /// </summary>
     void FixedUpdate()
-    {
-        Vector2 movement = new Vector2(movementX, movementY);
-        rigidBody.AddForce(movement * speed);
+    {   //Check gravity interaction with vector when let go of key
+        float movementX = playerInputX;
+        if (playerInputY > 0)   //If the key up is pressed
+        {
+            rigidBody.gravityScale = 0f;
+            var moveWithUp = new Vector2(movementX, 0);
+            rigidBody.AddForce(moveWithUp * glidingAcceleration);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -hoverForce);  //! Force the player to have only one speed
+        }
+        else
+        {
+            var movementY = playerInputY;
+            rigidBody.gravityScale = gravityForce;
+            var move = new Vector2 (movementX, movementY);
+            rigidBody.AddForce (move * glidingAcceleration);
+            Debug.Log($"The new velocity {move}");
+        }
+        rigidBody.velocity = Vector2.ClampMagnitude(rigidBody.velocity, maxMoveMagnitude);  //Clamp the speed so player would not move too fast
+        Debug.Log($"current velocity: {rigidBody.velocity.y} and horizontal: {rigidBody.velocity.x}");
     }
 }
