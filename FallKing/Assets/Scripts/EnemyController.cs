65159c0d89c164490c9d2f2b780d62427ef01936
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-05-26 13:57:47 -0700

    Add max distance enemy can move in each hop
    Also may need to work on the enemy landing part

--- FallKing/Assets/Scripts/EnemyController.cs
+++ FallKing/Assets/Scripts/EnemyController.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+
+////TODO: Add jump distance. Currently the user can just bounce straight to the target location without any stops
+//TODO: User can slide on the groudn after landing due to momentum. Think about leave it llike that or not.
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
@@ -10,16 +14,14 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float scanInterval;    //Time between scanning for target
     [SerializeField] private float moveInterval;    //Time between each move to target
     [SerializeField] private float timeToLocation;  //Time object should take to move to target location (exclude the interval)
-    [SerializeField] private float horizontalMoveForce;   //Move force toward the player horizontally
+    [SerializeField] private float oneHopDistance;   //The max distance user can travel in one hop
 
     private Rigidbody2D rigidBody;
     private RaycastHit2D hit;
-    private float horizontalDistToTarget = 0;
     private float scanTimer = 0;
     private float moveTimer = 0;
     private float initialJumpForce;
-
-    private float force;
+    private float impulseForce; // Force used for the user hopping - use Force.Impulse
 
     private void Start()
     {
@@ -52,30 +54,30 @@ public class EnemyController : MonoBehaviour
             Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
             Vector2 targetPos = new Vector2(player.transform.position.x, player.transform.position.y);
 
-            //////////
-            var velocity = (targetPos.x - currentPos.x) / timeToLocation;
-            force = (rigidBody.mass * velocity) / timeToLocation;
-            Debug.Log($"The force {force}");
-            //////////
+            float distToTarget = targetPos.x - currentPos.x;
+            distToTarget = Mathf.Clamp(distToTarget, -oneHopDistance, oneHopDistance);  //Can only travel max distance
+
+            var velocity = distToTarget / timeToLocation;
+            impulseForce = (rigidBody.mass * velocity) / timeToLocation;   // force = impulse * velocity / times
+            Debug.Log($"The force {impulseForce}");
 
             hit = Physics2D.Linecast(currentPos, targetPos);
-            horizontalDistToTarget = (targetPos - currentPos).normalized.x;
             scanTimer = 0f;
 
         }
         scanTimer += Time.deltaTime;
     }
 
-    //TODO: Change the physisc, when the target is too close, the force will make the user jump over the target over and over
-    //TODO: A better method would be letting the user indicate the time they want to reach target, and from there calculate the force need
+    ////TODO: Change the physisc, when the target is too close, the force will make the user jump over the target over and over/
+    ////TODO: A better method would be letting the user indicate the time they want to reach target, and from there calculate the force need
     private void FixedUpdate()
     {
         if (moveTimer > moveInterval)
         {
             if (hit.collider && hit.collider.gameObject.CompareTag("Player"))
             {
-                Vector2 hopToTarget = new Vector2(force, jumpForce);
-                rigidBody.AddForce(hopToTarget, ForceMode2D.Impulse);
+                Vector2 hopToTarget = new Vector2(impulseForce, jumpForce);
+                rigidBody.AddForce(hopToTarget, ForceMode2D.Impulse);   //? FIXME: Change to impulse made the user still jumping despite the jumpForce is 0
 
                 Debug.Log($"The force vector {hopToTarget}");
                 //Debug.Log($"The thing that was hit {hit.rigidbody.name}");
