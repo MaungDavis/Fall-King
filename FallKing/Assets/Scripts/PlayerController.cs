c663941823137314f0de887eb61c04df7d77f7ab
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-05-31 17:02:00 -0700

    Add some changes into playermovement

--- FallKing/Assets/Scripts/PlayerController.cs
+++ FallKing/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
+//Todo: When player release the key, force the user to stop (currently using add force make the user keep sliding)
+//TODO: The user will keep a minimal amount of momentum when stop?
+    //! Add a huge force to the opposite moving direction?
 public class PlayerController : MonoBehaviour
 {
+    [Header("Horizontal Physics")]
     [SerializeField] private float glidingAcceleration = 15f;
-    [SerializeField] private float hoverForce = 0.8f;
     [SerializeField] private float maxMoveMagnitude = 20f;
+    [Tooltip("Time from when release key for player to complete stop")]
+    [SerializeField] private float timeToStop = 1f;
+
+    [Header("Vertical Physics")]
+    [SerializeField] private float hoverForce = 0.8f;
     [SerializeField] private float maxFallMagnitude = 20f;
 
     private Rigidbody2D rigidBody;
     private float initialGravity;
     private float playerInputX, playerInputY;
+    bool playerRelasedKey = true;
 
     void Start()
     {
@@ -38,23 +47,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    /// <summary>
-    /// Up = only lower the gravity force
-    /// Player can only fall up to maximum speed
-    /// </summary>
     void FixedUpdate()
     {   ////TODO: When hover key pressed, fan physics doesn't work due to manually changing velocity in controll
         ////TODO: Stop using Add force to control player hovering speed? Instead change the gravity? Clamp the velocity as needed
         ////? Currently if the player is shoot up by fan while holding hover, the lesser gravity will allow to shoot up even further
-        float movementX = playerInputX;
+
+        // Player release moving key, add a force to the opposite moving direction
+        if (playerInputX == 0)  //TODO: To use impulse, make sure to only execute this block once per player key released. Or else it will apply over and over the whole time the key is not pressed
+        {
+            //! Reverse impulse? Wrong physics tho
+            var reverseImpulse = (rigidBody.mass * rigidBody.velocity.magnitude) / timeToStop;
+            Debug.Log($"The impulse stop force {reverseImpulse}");
+            rigidBody.AddForce(new Vector2(reverseImpulse * (-rigidBody.velocity.normalized.x), 0) * glidingAcceleration, ForceMode2D.Impulse);
+        }
+
         if (playerInputY > 0)   //If the key up is pressed and the player is falling
         {
             if (rigidBody.velocity.y < 0)
             {
                 rigidBody.gravityScale = initialGravity - hoverForce;
-                var moveWithUp = new Vector2(movementX, 0);
-                rigidBody.AddForce(moveWithUp * glidingAcceleration);
+                //var moveWithUp = new Vector2(movementX, 0); //? Seperate add force horizontal and vertical later
+                //rigidBody.AddForce(moveWithUp * glidingAcceleration);
                 //rigidBody.velocity = new Vector2(rigidBody.velocity.x, -hoverForce);  //! Force the player to have only one speed
             }
         }
@@ -62,10 +75,14 @@ public class PlayerController : MonoBehaviour
         {   ////! Allow player to hold down to go down faster -> Will be a problem since the downspeed is clamped below
             var movementY = playerInputY;
             rigidBody.gravityScale = initialGravity;
-            var move = new Vector2(movementX, movementY);
-            rigidBody.AddForce(move * glidingAcceleration);
+            var moveUp = new Vector2(0, movementY);
+            rigidBody.AddForce(moveUp);
             //Debug.Log($"The new velocity {move}");
         }
+
+        //! Now add horizontal force
+        float movementX = playerInputX;
+        rigidBody.AddForce(new Vector2(movementX, 0) * glidingAcceleration);
 
         //Clamp the falling speed AND left/right movement IF falling AND the player not holding down to go down faster
         if (rigidBody.velocity.y < 0 && playerInputY > 0)
